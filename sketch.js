fd5ba542ab4349ecc78af985c81952641f1a4255
jiya777 <72973968+jiya777@users.noreply.github.com>
2021-05-18 21:15:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) 
+    backgroundImg=loadImage(bg);
+
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+    getBackgroundImg();
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    var response=await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata")
+    var responsejson=await response.json();
+var datetime=responsejson.datetime;
+var hour=datetime.slice(11,13);
+console.log(hour);
+
+if(hour>=04&&hour<=6){
+    bg="sunrise1.png"
+}else if(hour>=06&&hour<=08){
+    bg="sunrise2.png";
+}else if(hour>=23&&hour==0){
+    bg="sunset10.png";
+}else if(hour==0&&hour<=3){
+    bg="sunset11.png";
+   
+}else{
+    bg="sunset12"
+}
+}
+
+
+
+
+
+
+
+
+    
+
